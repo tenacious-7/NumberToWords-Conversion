230343f8e0009bc0352b37e099992e162cb1e599
Durgesh Kumar Pandey <114678746+tenacious-7@users.noreply.github.com>
2023-03-26 11:27:13 +0530

    Add files via upload

---
+++ WebApplication1/WebApplication1/APICONTROLLER.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class APIController
+    {
+        [HttpGet]
+        [Route("api/NumberToWords/{number}")]
+        public string ConvertToWords(int number)
+        {
+            return NumberToWordsConverter.ConvertNumberToWords(number);
+        }
+
+    }
+}
---
+++ WebApplication1/WebApplication1/Controllers/NumberToWordsConverter.cs
@@ -0,0 +1,55 @@
+﻿namespace WebApplication1
+{
+    public class NumberToWordsConverter
+    {
+        public static string ConvertNumberToWords(int number)
+        {
+            if (number == 0)
+                return "zero";
+
+            if (number < 0)
+                return "minus " + ConvertNumberToWords(Math.Abs(number));
+
+            string words = "";
+
+            if ((number / 1000000) > 0)
+            {
+                words += ConvertNumberToWords(number / 1000000) + " million ";
+                number %= 1000000;
+            }
+
+            if ((number / 1000) > 0)
+            {
+                words += ConvertNumberToWords(number / 1000) + " thousand ";
+                number %= 1000;
+            }
+
+            if ((number / 100) > 0)
+            {
+                words += ConvertNumberToWords(number / 100) + " hundred ";
+                number %= 100;
+            }
+
+            if (number > 0)
+            {
+                if (words != "")
+                    words += "and ";
+
+                string[] unitsMap = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
+                string[] tensMap = { "zero", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" };
+
+                if (number < 20)
+                    words += unitsMap[number];
+                else
+                {
+                    words += tensMap[number / 10];
+                    if ((number % 10) > 0)
+                        words += "-" + unitsMap[number % 10];
+                }
+            }
+
+            return words;
+        }
+
+    }
+}
